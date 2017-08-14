ea3e9bbbb656610b2d811f4364fd3a2272476824
James Kong <james.kong@nixplay.com>
2017-08-14 10:09:10 +0800

    Refactoring

--- androidphotobrowser/src/main/java/com/creedon/androidphotobrowser/PhotoBrowserBasicActivity.java
+++ androidphotobrowser/src/main/java/com/creedon/androidphotobrowser/PhotoBrowserBasicActivity.java
@@ -46,7 +46,7 @@ public abstract class PhotoBrowserBasicActivity extends AppCompatActivity implem
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_demo);
+        setContentView(R.layout.activity_photobrowser);
 
 
     }
